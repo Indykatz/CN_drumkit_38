d9074186e68533c29877e0edc3900a4dc03228a3
Indykatz <cats83@me.com>
2022-05-24 18:34:39 +0100

    button drums

--- app.js
+++ app.js
@@ -1,38 +1,40 @@
 // JS for Drum kit
 
 // sounds
-const audioBoom = new Audio("sounds/boom.wav");
-const audioClap = new Audio("sounds/clap.wav");
-const audioHiHat = new Audio("sounds/hithat.wav");
-const audioKick = new Audio("sounds/kick.wav");
-const audioOpenHat = new Audio("sounds/openhat.wav");
-const audioride = new Audio("sounds/ride.wav");
-const audioSnare = new Audio("sounds/snare.wav");
-const audioTink = new Audio("sounds/tink.wav");
-const audioTom = new Audio("sounds/tom.wav");
+const Boom = new Audio("sounds/boom.wav");
+const Clap = new Audio("sounds/clap.wav");
+const HiHat = new Audio("sounds/hithat.wav");
+const Kick = new Audio("sounds/kick.wav");
+const OpenHat = new Audio("sounds/openhat.wav");
+const Ride = new Audio("sounds/ride.wav");
+const Snare = new Audio("sounds/snare.wav");
+const Tink = new Audio("sounds/tink.wav");
+const Tom = new Audio("sounds/tom.wav");
 
-const boom = document.getElementById("boom");
+let button = document.getElementById("button");
 
-boom.addEventListener("click", () => {
-  // tip from josh
-  audioBoom.pause();
-  audioBoom.load();
-  audioBoom.currentTime = 0;
-  audioBoom.volume = 1.0;
-  audioBoom.play();
+button.addEventListener("click", () => {
+  let list = document.get("drumList");
 });
 
-const clap = document.getElementById("clap");
-clap.addEventListener("click", () => {
-  audioClap.play();
-});
+// tip from josh
+//   audioBoom.pause();
+//   audioBoom.load();
+//   audioBoom.currentTime = 0;
+//   audioBoom.volume = 1.0;
+//   audioBoom.play();
 
-const hiHat = document.getElementById("hiHat");
-hiHat.addEventListener("click", () => {
-  audioHiHat.play();
-});
+// const clap = document.getElementById("clap");
+// clap.addEventListener("click", () => {
+//   audioClap.play();
+// });
 
-const kick = document.getElementById("kick");
-kick.addEventListener("click", () => {
-  audioKick.play();
-});
+// const hiHat = document.getElementById("hiHat");
+// hiHat.addEventListener("click", () => {
+//   audioHiHat.play();
+// });
+
+// const kick = document.getElementById("kick");
+// kick.addEventListener("click", () => {
+//   audioKick.play();
+// });
