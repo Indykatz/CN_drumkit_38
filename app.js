c17f215f3a5194e31b606575a793da2f96d5b038
Indykatz <cats83@me.com>
2022-05-24 15:01:00 +0100

    commit

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// Drum kit
+// JS for Drum kit
+
+const boom = document.getElementById("boom");
+const audioBoom = document.getElementById("audioBoom");
+
+boom.addEventListener("click", () => {
+  audioBoom.play();
+});
+
+const clap = document.getElementById("clap");
+const audioClap = document.getElementById("audioClap");
+
+clap.addEventListener("click", () => {
+  audioClap.play();
+});
+
+const hiHat = document.getElementById("hiHat");
+const audioHiHat = document.getElementById("audioHiHat");
+
+hiHat.addEventListener("click", () => {
+  audioHiHat.play();
+});
