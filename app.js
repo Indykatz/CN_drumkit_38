fe9188ebca497c16e183ca0dfae14a6f058f9da4
Indykatz <cats83@me.com>
2022-05-24 15:55:51 +0100

    commit 9 drums v1

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
 // JS for Drum kit
 
+// sounds
+const audioBoom = new Audio("sounds/boom.wav");
+const audioClap = new Audio("sounds/clap.wav");
+const audioHiHat = new Audio("sounds/hithat.wav");
+const audioKick = new Audio("sounds/kick.wav");
+const audioOpenHat = new Audio("sounds/openhat.wav");
+const audioride = new Audio("sounds/ride.wav");
+const audioSnare = new Audio("sounds/snare.wav");
+const audioTink = new Audio("sounds/tink.wav");
+const audioTom = new Audio("sounds/tom.wav");
+
 const boom = document.getElementById("boom");
-const audioBoom = document.getElementById("audioBoom");
 
 boom.addEventListener("click", () => {
+  // tip from josh
+  audioBoom.pause();
+  audioBoom.load();
+  audioBoom.currentTime = 0;
+  audioBoom.volume = 1.0;
   audioBoom.play();
 });
 
 const clap = document.getElementById("clap");
-const audioClap = document.getElementById("audioClap");
-
 clap.addEventListener("click", () => {
   audioClap.play();
 });
 
 const hiHat = document.getElementById("hiHat");
-const audioHiHat = document.getElementById("audioHiHat");
-
 hiHat.addEventListener("click", () => {
   audioHiHat.play();
 });
 
 const kick = document.getElementById("kick");
-const audioKick = document.getElementById("audioKick");
-
 kick.addEventListener("click", () => {
   audioKick.play();
 });
