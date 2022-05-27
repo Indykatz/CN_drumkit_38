1d100e5f53c65651d4ab520ff2471288f1322054
Indykatz <cats83@me.com>
2022-05-27 10:33:56 +0100

    button color

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 // JS for Drum kit
 
+// CLICK
 // List of drums
 const listItems = document.getElementsByTagName("li");
+// for each item in list item
+for (let listItem of listItems) {
+  // listne to click
+  listItem.addEventListener("click", () => {
+    // new string `sound/IDTAG/.way - example `sounds/boom.wav
+    let sound_file = `sounds/${listItem.id}.wav`;
+    // create new audio with path string
+    let drum_beat = new Audio(sound_file);
+    // play
+    drum_beat.play();
+  });
+}
 
+// KEYBOARD
 // list of path names to audio files
 const soundPath = [
   `sounds/boom.wav`,
@@ -18,23 +32,17 @@ const soundPath = [
 
 // list of keyboard key shortcut key
 const keysList = ["b", "c", "h", "k", "o", "r", "s", "t", "m"];
-
-// ACTIONS
-
-// click
-for (let listItem of listItems) {
-  listItem.addEventListener("click", () => {
-    let sound_file = `sounds/${listItem.id}.wav`;
-    let drum_beat = new Audio(sound_file);
-    drum_beat.play();
-  });
-}
-
 // keyboard
+// listen for key pressed event
 document.addEventListener("keypress", (e) => {
+  // for each item in keysList get index value not the element (i, 2, 3, 4,...)
   for (let i = 0; i < keysList.length; i++) {
+    // if keycode equals the valkues of the list index element
+    // index 1 = "b" index 1 = sound/boom.wav
     if (e.key == keysList[i]) {
+      // get the ssamae index number for ssound path
       let dropBeat = new Audio(soundPath[i]);
+      // drop that funky beat
       dropBeat.play();
     }
   }
